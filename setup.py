79a06338c97d29b0560d7c66df79e9ee5a9d0c15
Iliyas Jorio <iliyas@jor.io>
2020-06-01 22:57:51 +0200

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='GitFourchette',
+    version='0.1',
+    package_dir={'': 'GitFourchette'},
+    python_requires='>=3',
+    install_requires=[
+        'PySide2>=5',
+        'GitPython>=3',
+        'psutil'
+    ],
+)
