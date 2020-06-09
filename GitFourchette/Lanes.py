a7cfd5b6b7f39658a0cc6ad6cdf25935fc0eaa84
Iliyas Jorio <iliyas@jor.io>
2020-06-09 08:04:54 +0200

    Reuse freed up lanes

--- GitFourchette/Lanes.py
+++ GitFourchette/Lanes.py
@@ -11,9 +11,10 @@ class Lanes:
         if 0 == len(self.lanes):  # first call: put first commit in first lane
             self.lanes.append(commit)
 
-        extraParentLanes = max(0, len(parents) - 1)
+        result = [0] * len(self.lanes)
 
-        result = [0] * (len(self.lanes) + extraParentLanes)
+        # cached index of the first free lane
+        freeLane = len(self.lanes)
 
         myLane = self.lanes.index(commit)
 
@@ -23,20 +24,32 @@ class Lanes:
                 self.lanes[i] = None
             elif lane is not None:
                 result[i] |= Lanes.STRAIGHT
+            # cache first free lane (re-test self.lanes[i] because we might have freed a lane above)
+            if self.lanes[i] is None and i < freeLane:
+                freeLane = i
 
         # continue straight line
         if len(parents) > 0:
             self.lanes[myLane] = parents[0]
             result[myLane] |= Lanes.FORK_DOWN
 
+        # add new branches
+        for parent in parents[1:]:
+            # find a free lane
+            try:
+                freeLane = self.lanes.index(None, freeLane)
+            except ValueError:
+                # all lanes taken: add new lane
+                freeLane = len(self.lanes)
+                self.lanes.append(None)
+                result.append(0)
+            # fill it in
+            self.lanes[freeLane] = parent
+            result[freeLane] |= Lanes.FORK_DOWN
+
         # compact free lanes on the right
         while len(self.lanes) > 0 and self.lanes[-1] is None:
-            self.lanes.pop()
-
-        # add new branches outwards
-        for parent in parents[1:]:
-            result[len(self.lanes)] |= Lanes.FORK_DOWN
-            self.lanes.append(parent)
+            del self.lanes[-1]
 
         return myLane, result
 
