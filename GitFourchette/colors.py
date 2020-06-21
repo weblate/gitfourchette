3c57a63c88832ce935d6bf38bf637fb00de08edc
Iliyas Jorio <iliyas@jor.io>
2020-06-21 20:42:15 +0200

    Color scheme

---
+++ GitFourchette/colors.py
@@ -0,0 +1,33 @@
+# Color scheme based on https://clrs.cc/
+
+from PySide2.QtGui import QColor
+
+navy    = QColor(0x001F3F)
+blue    = QColor(0x0074D9)
+aqua    = QColor(0x7FDBFF)
+teal    = QColor(0x39CCCC)
+olive   = QColor(0x3D9970)
+green   = QColor(0x2ECC40)
+lime    = QColor(0x01FF70)
+yellow  = QColor(0xFFDC00)
+orange  = QColor(0xFF851B)
+red     = QColor(0xFF4136)
+fuchsia = QColor(0xF012BE)
+purple  = QColor(0xB10DC9)
+maroon  = QColor(0x85144B)
+white   = QColor(0xFFFFFF)
+silver  = QColor(0xDDDDDD)
+gray    = QColor(0xAAAAAA)
+black   = QColor(0x111111)
+
+rainbow = [
+    navy, blue, aqua, teal, olive, green, lime, yellow, orange, red, maroon, fuchsia, purple
+]
+
+rainbowLight = [
+    orange, red, maroon, fuchsia, purple, blue, aqua, teal, olive, green, lime, yellow
+]
+
+grayscale = [
+    black, gray, silver, white
+]
