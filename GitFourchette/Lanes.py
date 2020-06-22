6b13c0eea8fa22fa016d49e32518e42e2b8facd4
Iliyas Jorio <iliyas@jor.io>
2020-06-22 07:00:00 +0200

    Lane algo perf improvement

--- GitFourchette/Lanes.py
+++ GitFourchette/Lanes.py
@@ -1,57 +1,76 @@
+from zlib import crc32
+
+
 class Lanes:
     FORK_UP = 1
     FORK_DOWN = 2
     STRAIGHT = 4
 
-    def __init__(self):
+    def __init__(self, withChecksum=False):
         self.lanes = []
-
-    def _findFreeLane(self, result, startAt=0) -> int:
-        try:
-            return self.lanes.index(None, startAt)
-        except ValueError:
-            # all lanes taken: create one on the right
-            free = len(self.lanes)
-            self.lanes.append(None)
-            result.append(0)
-            return free
+        self.draw = []
+        self.withChecksum = withChecksum
+        self.check = 0
 
     def step(self, commit, parents):
-        result = [0] * len(self.lanes)
+        lanes = self.lanes
 
+        def findFreeLane(startAt=0) -> int:
+            try:
+                return lanes.index(None, startAt)
+            except ValueError:
+                # all lanes taken: create one on the right
+                free = len(lanes)
+                lanes.append(None)
+                paint.append(0)
+                return free
+
+        # start with all-straight lines
+        # this is faster than starting with nothing and then adding straight lines
+        paint = [Lanes.STRAIGHT] * len(lanes)
+
+        # look up my lane.
         # the main lane for this commit is the leftmost lane reserved for it
         try:
-            myLane = self.lanes.index(commit)
+            myLane = lanes.index(commit)
         except ValueError:
             # this commit is the tip of a branch
-            myLane = self._findFreeLane(result)
+            myLane = findFreeLane()
 
-        for i, lane in enumerate(self.lanes):
-            if lane == commit:
+        # comb through lanes and update draw data
+        # (this is where the bulk of the time is spent in this function)
+        for i, lane in enumerate(lanes):
+            if lane is None:
+                paint[i] = 0
+            elif lane == commit:
                 # tie up loose ends
-                result[i] |= Lanes.FORK_UP
-                self.lanes[i] = None
-            elif lane is not None:
-                # continue parallel lane that isn't mine
-                result[i] |= Lanes.STRAIGHT
+                paint[i] = Lanes.FORK_UP
+                lanes[i] = None  # free up lane
 
         # hand over my main lane to my first parent
         if len(parents) > 0:
-            self.lanes[myLane] = parents[0]
-            result[myLane] |= Lanes.FORK_DOWN
+            lanes[myLane] = parents[0]
+            paint[myLane] |= Lanes.FORK_DOWN
 
         # add new branches for my other parents
         freeLane = 0
         for parent in parents[1:]:
             # find a free lane or create one if none is available
-            freeLane = self._findFreeLane(result, freeLane)
+            freeLane = findFreeLane(freeLane)
             # fill it in
-            self.lanes[freeLane] = parent
-            result[freeLane] |= Lanes.FORK_DOWN
+            lanes[freeLane] = parent
+            paint[freeLane] |= Lanes.FORK_DOWN
 
         # compact free lanes on the right
-        while len(self.lanes) > 0 and self.lanes[-1] is None:
-            del self.lanes[-1]
+        while len(lanes) > 0 and lanes[-1] is None:
+            del lanes[-1]
 
-        return myLane, result
+        if self.withChecksum:
+            self.updateChecksum(paint)
 
+        return myLane, paint
+
+    def updateChecksum(self, result):
+        self.check = crc32(len(result).to_bytes(4, 'little'), self.check)
+        for r in result:
+            self.check = crc32(r.to_bytes(1, 'little'), self.check)
