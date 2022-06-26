d9d57bb928602b5aac8271081892f6fb04057c40
Iliyas Jorio <iliyas@jor.io>
2022-06-26 14:34:54 +0200

    Update directories to watch when a dir is created or deleted

--- gitfourchette/filewatcher.py
+++ gitfourchette/filewatcher.py
@@ -5,8 +5,14 @@ from gitfourchette import log
 from gitfourchette.qt import *
 
 
-def iterateWatchList(repo):
-    frontier = [repo.workdir]
+TAG = "FSW"
+
+
+def walkWatchableDirs(repo: pygit2.Repository, startDir=""):
+    if not startDir:
+        startDir = os.path.normpath(repo.workdir)
+
+    frontier = [startDir]
 
     while frontier:
         parent = frontier.pop(0)
@@ -21,21 +27,68 @@ def iterateWatchList(repo):
 class FileWatcher(QObject):
     changeDetected = Signal()
 
-    def __init__(self, repo: pygit2.Repository):
+    repo: pygit2.Repository
+    fsw: QFileSystemWatcher
+    rewatchDelay: QTimer
+    pendingRewatch: set[str]
+
+    def __init__(self, repo: pygit2.Repository, delayMilliseconds=100):
         super().__init__(None)
+
+        self.repo = repo
+
         self.fsw = QFileSystemWatcher()
-        self.fsw.addPaths(iterateWatchList(repo))
+        self.fsw.addPaths(walkWatchableDirs(self.repo))
+
         self.fsw.directoryChanged.connect(self.onDirectoryChanged)
         self.fsw.fileChanged.connect(self.onFileChanged)
-        self.dirty = False
 
-    def onDirectoryChanged(self, path: str):
-        log.info("FSW", f"Directory changed: {path}")
-        self.dirty = True
+        self.pendingRewatch = set()
+
+        self.rewatchDelay = QTimer(self)
+        self.rewatchDelay.setSingleShot(True)
+        self.rewatchDelay.setInterval(delayMilliseconds)
+        self.rewatchDelay.timeout.connect(self.onRewatchTimeout)
+
+    def prettifyPathList(self, pathList):
+        prefix = os.path.normpath(self.repo.workdir) + "/"
+        return [d.removeprefix(prefix) for d in pathList]
+
+    def refreshDirectory(self, path: str):
+        assert path == os.path.normpath(path)
+
+        recursiveChildren = {d for d in self.fsw.directories()
+                             if d.startswith(path)}
+
+        directChildren = {d for d in recursiveChildren
+                          if os.path.split(d)[0] == path}
+
+        for childItem in os.listdir(path):
+            fullChildPath = os.path.join(path, childItem)
+            if (os.path.isdir(fullChildPath)
+                    and fullChildPath not in directChildren
+                    and not self.repo.path_is_ignored(fullChildPath)):
+                log.info(TAG, f"Watching new directory: {fullChildPath}")
+                self.fsw.addPaths(walkWatchableDirs(self.repo, fullChildPath))
+
+        zombieChildren = [d for d in recursiveChildren if not os.path.isdir(d)]
+        if zombieChildren:
+            log.info(TAG, f"Removing zombie children: {self.prettifyPathList(zombieChildren)}")
+            self.fsw.removePaths(zombieChildren)
+
+    def onRewatchTimeout(self):
+        log.info(TAG, f"Directories changed: {self.prettifyPathList(self.pendingRewatch)}")
+        for d in self.pendingRewatch:
+            self.refreshDirectory(d)
+        self.pendingRewatch.clear()
+        log.info(TAG, f"Watched: {self.prettifyPathList(self.fsw.directories())}")
         self.changeDetected.emit()
 
+    def onDirectoryChanged(self, path: str):
+        self.pendingRewatch.add(path)
+        self.rewatchDelay.start()
+
     def onFileChanged(self, path: str):
-        log.info("FSW", f"File changed: {path}")
-        self.dirty = True
+        log.info(TAG, f"File changed: {path}")
         self.changeDetected.emit()
 
