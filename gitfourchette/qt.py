a6c918bfa1baae2166d0a8fd1bfd3437837dbc7d
Iliyas Jorio <iliyas@jor.io>
2022-10-22 21:02:15 +0200

    Allow bypassing qtpy

--- gitfourchette/qt.py
+++ gitfourchette/qt.py
@@ -8,8 +8,19 @@
 #       pyqt5
 # If you're running unit tests, use the PYTEST_QT_API environment variable instead.
 
-from qtpy.QtCore import *
-from qtpy.QtWidgets import *
-from qtpy.QtGui import *
-from qtpy import API_NAME as qtBindingName
-from qtpy.QtCore import __version__ as qtBindingVersion
+import os
+
+if "pyside6" == os.environ.get("QT_API", "").lower():
+    # If we're making a PyInstaller build, qtpy will try to pull in QtOpenGL and other bloat.
+    # So, bypass qtpy and import PySide6 manually.
+    from PySide6.QtCore import *
+    from PySide6.QtWidgets import *
+    from PySide6.QtGui import *
+    from PySide6 import __version__ as qtBindingVersion
+    qtBindingName = "PySide6"
+else:
+    from qtpy.QtCore import *
+    from qtpy.QtWidgets import *
+    from qtpy.QtGui import *
+    from qtpy import API_NAME as qtBindingName
+    from qtpy.QtCore import __version__ as qtBindingVersion
