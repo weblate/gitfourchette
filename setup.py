59a4e18cfd392b1fd5233257e020ba7f413a0bfe
Iliyas Jorio <iliyas@jor.io>
2022-11-09 19:54:22 +0100

    Refresh packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 from setuptools import setup
 
 setup(
-    name='GitFourchette',
+    name='gitfourchette',
     version='0.1',
-    package_dir={'': 'gitfourchette'},
+    description='The comfy Git UI',
+    author='Iliyas Jorio',
+    url='https://github.com/jorio/gitfourchette',
+    classifiers=[
+        'Topic :: Software Development :: Version Control :: Git',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: Developers',
+    ],
+    package_dir={
+        'gitfourchette': 'gitfourchette'
+    },
+    package_data={
+        'gitfourchette.assets': ['*']
+    },
     python_requires='>=3',
     install_requires=[
         'qtpy',
         'pygit2>=1.11.0',
-        'psutil',
     ],
+    extras_require={
+        'memory-indicator': ['psutil'],
+    },
     tests_require=[
         'pytest',
         'pytest-qt',
