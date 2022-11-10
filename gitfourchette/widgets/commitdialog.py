8185a089959d762a5516bcdd2bf26533ea8c91d7
Iliyas Jorio <iliyas@jor.io>
2022-11-10 15:43:19 +0100

    Commit dialog: check signature validity

--- gitfourchette/widgets/commitdialog.py
+++ gitfourchette/widgets/commitdialog.py
@@ -5,6 +5,10 @@ from pygit2 import Signature
 
 
 class CommitDialog(QDialog):
+    @property
+    def acceptButton(self) -> QPushButton:
+        return self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok)
+
     def __init__(
             self,
             initialText: str,
@@ -17,13 +21,8 @@ class CommitDialog(QDialog):
         self.ui = Ui_CommitDialog()
         self.ui.setupUi(self)
 
-        acceptButton = self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok)
-        counterLabel = self.ui.counterLabel
-        summaryEditor = self.ui.summaryEditor
-        descriptionEditor = self.ui.descriptionEditor
-
         # Make summary text edit font larger
-        tweakWidgetFont(summaryEditor, 150)
+        tweakWidgetFont(self.ui.summaryEditor, 150)
 
         if isAmend:
             prompt = self.tr("Amend commit message")
@@ -36,30 +35,42 @@ class CommitDialog(QDialog):
 
         self.ui.authorSignature.setSignature(authorSignature)
 
-        acceptButton.setText(buttonCaption)
-        summaryEditor.setPlaceholderText(prompt)
+        self.acceptButton.setText(buttonCaption)
+        self.ui.summaryEditor.setPlaceholderText(prompt)
 
-        counterLabel.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
-        counterLabel.setMinimumWidth(counterLabel.fontMetrics().horizontalAdvance('000'))
-        counterLabel.setMaximumWidth(counterLabel.minimumWidth())
+        self.ui.counterLabel.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        self.ui.counterLabel.setMinimumWidth(self.ui.counterLabel.fontMetrics().horizontalAdvance('000'))
+        self.ui.counterLabel.setMaximumWidth(self.ui.counterLabel.minimumWidth())
 
-        def onSummaryChanged():
-            text = summaryEditor.text()
-            hasText = bool(text.strip())
-            counterLabel.setText(str(len(text)))
-            acceptButton.setEnabled(hasText)
-        summaryEditor.textChanged.connect(onSummaryChanged)
+        self.ui.summaryEditor.textChanged.connect(self.updateCounterLabel)
+        self.ui.summaryEditor.textChanged.connect(self.updateAcceptButton)
+        self.ui.revealAuthor.stateChanged.connect(self.updateAcceptButton)
+        self.ui.authorSignature.signatureChanged.connect(self.updateAcceptButton)
 
         split = initialText.split('\n', 1)
         if len(split) >= 1:
-            summaryEditor.setText(split[0].strip())
+            self.ui.summaryEditor.setText(split[0].strip())
         if len(split) >= 2:
-            descriptionEditor.setPlainText(split[1].strip())
+            self.ui.descriptionEditor.setPlainText(split[1].strip())
 
-        onSummaryChanged()
+        self.updateCounterLabel()
+        self.updateAcceptButton()
 
         self.ui.revealAuthor.setChecked(False)
         self.ui.authorGroupBox.setVisible(False)
+
+    def updateCounterLabel(self):
+        text = self.ui.summaryEditor.text()
+        self.ui.counterLabel.setText(str(len(text)))
+
+    def updateAcceptButton(self):
+        self.acceptButton.setEnabled(self.canProceed())
+
+    def canProceed(self):
+        text = self.ui.summaryEditor.text()
+        hasAnyText = bool(text.strip())
+        signatureValid = not self.ui.revealAuthor.isChecked() or self.ui.authorSignature.isValid()
+        return hasAnyText and signatureValid
 
     def hasNonBlankSummary(self):
         return bool(self.ui.summaryEditor.text().strip())
--- gitfourchette/widgets/signatureform.py
+++ gitfourchette/widgets/signatureform.py
@@ -4,16 +4,28 @@ from pygit2 import Signature
 
 
 class SignatureForm(QWidget):
+    signatureChanged = Signal()
+
     def __init__(self, parent):
         super().__init__(parent)
         self.ui = Ui_SignatureForm()
         self.ui.setupUi(self)
+
+        self.ui.nameEdit.textChanged.connect(self.signatureChanged)
+        self.ui.emailEdit.textChanged.connect(self.signatureChanged)
+        self.ui.timeEdit.timeChanged.connect(self.signatureChanged)
 
     def setSignature(self, signature: Signature):
         qdt: QDateTime = QDateTime.fromSecsSinceEpoch(signature.time, Qt.TimeSpec.OffsetFromUTC, signature.offset * 60)
         self.ui.nameEdit.setText(signature.name)
         self.ui.emailEdit.setText(signature.email)
         self.ui.timeEdit.setDateTime(qdt)
+
+    def isValid(self) -> bool:
+        # pygit2 will raise GitError if either the author or the email is empty or whitespace-only
+        hasAuthor = bool(self.ui.nameEdit.text().strip())
+        hasEmail = bool(self.ui.emailEdit.text().strip())
+        return hasAuthor and hasEmail
 
     def getSignature(self) -> Signature:
         qdt = self.ui.timeEdit.dateTime()
