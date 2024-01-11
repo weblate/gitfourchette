9eb3cff5d9e159b5381ff06c80d914964bc1475c
Iliyas Jorio <iliyas@jor.io>
2024-01-11 19:40:24 +0100

    run.sh: readlink

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 
 PYTHON=${PYTHON:-python3}
-export PYTHONPATH="$(dirname "$0")"
+export PYTHONPATH="$(dirname "$(readlink -f -- "$0")" )"
 export QT_API=${QT_API:-pyqt6}
 
 $PYTHON -m gitfourchette "$@"
