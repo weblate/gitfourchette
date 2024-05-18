24c9aa4e7c1524b2b48b52432536aafb39322d6b
Iliyas Jorio <iliyas@jor.io>
2024-05-18 22:01:41 +0200

    Move FileList tests to their own file

--- test/test_gitfourchette.py
+++ test/test_gitfourchette.py
@@ -44,46 +44,6 @@ def testDisplayAllNestedUntrackedFiles(tempDir, mainWindow):
     assert qlvGetRowData(rw.stagedFiles) == []
 
 
-def testParentlessCommitFileList(tempDir, mainWindow):
-    wd = unpackRepo(tempDir)
-    rw = mainWindow.openRepo(wd)
-
-    oid = Oid(hex="42e4e7c5e507e113ebbb7801b16b52cf867b7ce1")
-    rw.jump(NavLocator.inCommit(oid))
-    assert qlvGetRowData(rw.committedFiles) == ["c/c1.txt"]
-
-
-def testSaveOldRevision(tempDir, mainWindow):
-    wd = unpackRepo(tempDir)
-    rw = mainWindow.openRepo(wd)
-
-    oid = Oid(hex="6462e7d8024396b14d7651e2ec11e2bbf07a05c4")
-    loc = NavLocator.inCommit(oid, "c/c2.txt")
-    rw.jump(NavLocator.inCommit(oid, "c/c2.txt"))
-    assert loc.isSimilarEnoughTo(rw.navLocator)
-    rw.committedFiles.saveRevisionAs(saveInto=tempDir.name)
-
-    with open(F"{tempDir.name}/c2@6462e7d.txt", "rb") as f:
-        contents = f.read()
-        assert contents == b"c2\n"
-
-
-def testSaveOldRevisionOfDeletedFile(tempDir, mainWindow):
-    wd = unpackRepo(tempDir)
-    rw = mainWindow.openRepo(wd)
-
-    commitOid = Oid(hex="c9ed7bf12c73de26422b7c5a44d74cfce5a8993b")
-
-    rw.jump(NavLocator.inCommit(commitOid))
-    assert qlvGetRowData(rw.committedFiles) == ["c/c2-2.txt"]
-    rw.committedFiles.selectRow(0)
-
-    # c2-2.txt was deleted by the commit.
-    # Expect GF to warn us about it.
-    rw.committedFiles.saveRevisionAs(saveInto=tempDir.name, beforeCommit=False)
-    acceptQMessageBox(rw, r"file.+deleted by.+commit")
-
-
 def testUnloadRepoWhenFolderGoesMissing(tempDir, mainWindow):
     wd = unpackRepo(tempDir)
     rw = mainWindow.openRepo(wd)
@@ -141,24 +101,6 @@ def testSkipRenameDetection(tempDir, mainWindow):
     assert not rw.diffBanner.isVisibleTo(rw)
 
 
-@pytest.mark.parametrize("context", [NavContext.UNSTAGED, NavContext.STAGED])
-def testRefreshKeepsMultiFileSelection(tempDir, mainWindow, context):
-    wd = unpackRepo(tempDir)
-    N = 10
-    for i in range(N):
-        writeFile(f"{wd}/UNSTAGED{i}", f"dirty{i}")
-        writeFile(f"{wd}/STAGED{i}", f"staged{i}")
-    with RepoContext(wd) as repo:
-        repo.index.add_all([f"STAGED{i}" for i in range(N)])
-        repo.index.write()
-
-    rw = mainWindow.openRepo(wd)
-    fl = rw.fileListByContext(context)
-    fl.selectAll()
-    rw.refreshRepo()
-    assert list(fl.selectedPaths()) == [f"{context.name}{i}" for i in range(N)]
-
-
 def testPrefsDialog(tempDir, mainWindow):
     # Open a repo so that refreshPrefs functions are exercized in coverage
     wd = unpackRepo(tempDir)
@@ -167,6 +109,11 @@ def testPrefsDialog(tempDir, mainWindow):
     def openPrefs() -> PrefsDialog:
         triggerMenuAction(mainWindow.menuBar(), "file/preferences")
         return findQDialog(mainWindow, "preferences")
+
+    # Open prefs, reset to first tab to prevent spillage from any previous test
+    dlg = openPrefs()
+    dlg.tabs.setCurrentIndex(0)
+    dlg.reject()
 
     # Open prefs, navigate to some tab and reject
     dlg = openPrefs()
@@ -467,99 +414,3 @@ def testAllTaskNamesTranslated(mainWindow):
                     and type is not tasks.RepoTask
                     and type not in tasks.TaskBook.names):
                 assert False, f"Missing task name translation for {key}"
-
-
-def testSearchFileList(mainWindow, tempDir):
-    oid = Oid(hex="83834a7afdaa1a1260568567f6ad90020389f664")
-
-    wd = unpackRepo(tempDir)
-    rw = mainWindow.openRepo(wd)
-
-    rw.jump(NavLocator.inCommit(oid))
-    assert rw.committedFiles.isVisibleTo(rw)
-    rw.committedFiles.setFocus()
-    QTest.keySequence(rw, "Ctrl+F")
-    QTest.qWait(0)
-
-    fileList = rw.committedFiles
-    searchBar = fileList.searchBar
-    assert searchBar.isVisibleTo(rw)
-    searchBar.lineEdit.setText(".txt")
-    QTest.qWait(0)
-    assert not searchBar.isRed()
-
-    assert qlvGetSelection(fileList) == ["a/a1.txt"]
-    QTest.keySequence(rw, "F3")
-    assert qlvGetSelection(fileList) == ["a/a2.txt"]
-    QTest.keySequence(rw, "F3")
-    assert qlvGetSelection(fileList) == ["master.txt"]
-    QTest.keySequence(rw, "F3")
-    assert qlvGetSelection(fileList) == ["a/a1.txt"]
-    QTest.keySequence(rw, "Shift+F3")
-    assert qlvGetSelection(fileList) == ["master.txt"]
-
-    searchBar.lineEdit.setText("a2")
-    QTest.qWait(0)
-    assert qlvGetSelection(fileList) == ["a/a2.txt"]
-
-    searchBar.lineEdit.setText("bogus")
-    QTest.qWait(0)
-    assert searchBar.isRed()
-
-    QTest.keySequence(searchBar, "Escape")
-    QTest.qWait(0)
-    assert not searchBar.isVisibleTo(rw)
-
-
-def testEditFileInExternalEditor(mainWindow, tempDir):
-    wd = unpackRepo(tempDir)
-    rw = mainWindow.openRepo(wd)
-    rw.jump(NavLocator.inCommit(Oid(hex="49322bb17d3acc9146f98c97d078513228bbf3c0"), "a/a1"))
-
-    editorPath = getTestDataPath("editor-shim.sh")
-    scratchPath = f"{tempDir.name}/external editor scratch file.txt"
-
-    # First, set the editor to an incorrect command to go through the "locate" code path
-    mainWindow.onAcceptPrefsDialog({"externalEditor": f'"{editorPath}-BOGUSCOMMAND" "{scratchPath}"'})
-    triggerMenuAction(rw.committedFiles.makeContextMenu(), "edit in editor-shim/current version")
-    QTest.qWait(1)
-    qmb = findQMessageBox(mainWindow, "n.t start text editor")
-    qmb.button(QMessageBox.StandardButton.Open).click()  # click "locate" button
-    # Set correct command; this must retain the arguments from the incorrect command
-    acceptQFileDialog(mainWindow, "locate.+editor-shim", editorPath)
-
-    # Now open the file in our shim
-    # HEAD revision
-    triggerMenuAction(rw.committedFiles.makeContextMenu(), "edit in editor-shim/current")
-    assert b"a/a1" in readFile(scratchPath, timeout=1000)
-    Path(scratchPath).unlink()
-
-    # New revision
-    triggerMenuAction(rw.committedFiles.makeContextMenu(), "edit in editor-shim/before 49322bb")
-    acceptQMessageBox(mainWindow, "file did.?n.t exist")
-
-    # Old revision
-    triggerMenuAction(rw.committedFiles.makeContextMenu(), "edit in editor-shim/at 49322bb")
-    assert b"a1@49322bb" in readFile(scratchPath, timeout=1000)
-    Path(scratchPath).unlink()
-
-
-def testEditFileInExternalDiffTool(mainWindow, tempDir):
-    wd = unpackRepo(tempDir)
-    rw = mainWindow.openRepo(wd)
-    rw.jump(NavLocator.inCommit(Oid(hex="7f822839a2fe9760f386cbbbcb3f92c5fe81def7"), "b/b2.txt"))
-
-    editorPath = getTestDataPath("editor-shim.sh")
-    scratchPath = f"{tempDir.name}/external editor scratch file.txt"
-
-    # First, set the diff tool to an empty command to go through the "set up" code path
-    mainWindow.onAcceptPrefsDialog({"externalDiff": ""})
-    triggerMenuAction(rw.committedFiles.makeContextMenu(), "compare in.+diff tool")
-    acceptQMessageBox(mainWindow, "diff tool.+n.t set up")
-    findQDialog(mainWindow, "preferences").reject()
-
-    mainWindow.onAcceptPrefsDialog({"externalDiff": f'"{editorPath}" "{scratchPath}" $L $R'})
-    triggerMenuAction(rw.committedFiles.makeContextMenu(), "compare in editor-shim")
-    QTest.qWait(1)
-    assert b"[OLD]b2.txt" in readFile(scratchPath)
-    assert b"[NEW]b2.txt" in readFile(scratchPath)
