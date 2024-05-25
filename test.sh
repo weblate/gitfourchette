cdcefd5578dd255917fbe34ebb2a9df244fe3777
Iliyas Jorio <iliyas@jor.io>
2024-05-25 23:28:57 +0200

    Switch to pytest-cov (for faster coverage with pytest-xdist)

--- test.sh
+++ test.sh
@@ -6,22 +6,16 @@ HERE="$(dirname "$(readlink -f -- "$0")" )"
 PYTHON=${PYTHON:-python3}
 export PYTEST_QT_API=${PYTEST_QT_API:-pyqt6}
 export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-offscreen}
-COVERAGE=0
+
+RUNNER="$PYTHON -m pytest -n auto --dist worksteal"
 
 if [[ $1 = "--cov" ]]; then
     shift
-    RUNNER="$PYTHON -m coverage run -m pytest"
-    COVERAGE=1
+    RUNNER+=" --cov=gitfourchette --cov-report=html"
 else
     echo "Coverage report disabled, pass --cov to enable"
-    RUNNER="$PYTHON -m pytest"
 fi
 
 cd "$HERE"
 $RUNNER "$@"
 
-if [[ $COVERAGE -ne 0 ]]; then
-    # Generate HTML coverage report
-    $PYTHON -m coverage report | grep TOTAL
-    $PYTHON -m coverage html
-fi
