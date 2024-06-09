262c8b1bb91a4a69083d505be8603e0deff0623c
Iliyas Jorio <iliyas@jor.io>
2024-06-09 07:28:36 +0200

    QComboBoxWithPreview: More breathing room for preview

--- gitfourchette/toolbox/qcomboboxwithpreview.py
+++ gitfourchette/toolbox/qcomboboxwithpreview.py
@@ -4,33 +4,12 @@ from gitfourchette.qt import *
 class QComboBoxWithPreview(QComboBox):
     dataPicked = Signal(object)
 
-    class ItemDelegate(QStyledItemDelegate):
-        def paint(self, painter, option, index):
-            super().paint(painter, option, index)
-            painter.save()
-
-            pw: QComboBoxWithPreview = self.parent()
-            rect = QRect(option.rect)
-            rect.setLeft(rect.left() + pw.longestCaptionWidth + 16)
-
-            isSelected = bool(option.state & QStyle.StateFlag.State_Selected)
-            if not isSelected:
-                painter.setPen(option.palette.color(QPalette.ColorGroup.Normal, QPalette.ColorRole.PlaceholderText))
-            else:
-                painter.setPen(option.palette.color(QPalette.ColorGroup.Normal, QPalette.ColorRole.HighlightedText))
-
-            preview = index.data(Qt.ItemDataRole.UserRole + 1)
-
-            painter.drawText(rect, Qt.AlignmentFlag.AlignVCenter, preview)
-            painter.restore()
-
     def __init__(self, parent: QWidget):
         super().__init__(parent)
         self.numPresets = 0
-        self.longestCaptionWidth = 0
-        self.longestDataWidth = 0
-        self.longestPreviewWidth = 0
-        delegate = QComboBoxWithPreview.ItemDelegate(self)
+        self.captionWidth = 0
+        self.previewWidth = 0
+        delegate = QComboBoxWithPreviewDelegate(self)
         self.setItemDelegate(delegate)
         self.activated.connect(self.onActivated)
 
@@ -39,15 +18,16 @@ class QComboBoxWithPreview(QComboBox):
         self.addItem(caption)
         self.setItemData(i, data, Qt.ItemDataRole.UserRole + 0)
         self.setItemData(i, preview, Qt.ItemDataRole.UserRole + 1)
-        self.longestCaptionWidth = max(self.longestCaptionWidth, self.fontMetrics().horizontalAdvance(caption))
-        self.longestDataWidth    = max(self.longestDataWidth,    self.fontMetrics().horizontalAdvance(str(data)))
-        self.longestPreviewWidth = max(self.longestPreviewWidth, self.fontMetrics().horizontalAdvance(preview))
-        # if self.isEditable():
-        #     self.setMinimumWidth(self.longestDataWidth + 32)
+
+        fontMetrics = self.fontMetrics()
+        self.captionWidth = max(self.captionWidth, fontMetrics.horizontalAdvance(caption) + 20)
+        self.previewWidth = max(self.previewWidth, fontMetrics.horizontalAdvance(preview) + 6)
+
         self.numPresets += 1
 
     def showPopup(self):
-        self.view().setMinimumWidth(self.longestCaptionWidth + self.longestPreviewWidth + 16)
+        # TODO: Where is QListView padding defined?
+        self.view().setMinimumWidth(3 + self.captionWidth + self.previewWidth + 3)
         super().showPopup()
 
     def onActivated(self, index: int):
@@ -61,3 +41,26 @@ class QComboBoxWithPreview(QComboBox):
 
         if self.isEditable():
             self.setEditText(str(data))
+
+
+class QComboBoxWithPreviewDelegate(QStyledItemDelegate):
+    def paint(self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex):
+        super().paint(painter, option, index)
+        painter.save()
+
+        pw: QComboBoxWithPreview = self.parent()
+        rect = QRect(option.rect)
+        rect.setLeft(rect.left() + 3 + pw.captionWidth)
+
+        isSelected = bool(option.state & QStyle.StateFlag.State_Selected)
+        colorRole = QPalette.ColorRole.PlaceholderText if not isSelected else QPalette.ColorRole.HighlightedText
+
+        font: QFont = painter.font()
+        font.setItalic(True)
+
+        preview = index.data(Qt.ItemDataRole.UserRole + 1)
+
+        painter.setFont(font)
+        painter.setPen(option.palette.color(QPalette.ColorGroup.Normal, colorRole))
+        painter.drawText(rect, Qt.AlignmentFlag.AlignVCenter, str(preview))
+        painter.restore()
