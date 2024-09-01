17e3f9a78ebf0ed3497841b0c3c7e9e3e13bc159
Iliyas Jorio <iliyas@jor.io>
2024-09-01 15:47:38 +0200

    test.sh: Enforce QT_QPA_PLATFORM=offscreen
    Some environments (sddm?) may set this to variable to "xcb".

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -e
 HERE="$(dirname "$(readlink -f -- "$0")" )"
 PYTHON=${PYTHON:-python3}
 export PYTEST_QT_API=${PYTEST_QT_API:-pyqt6}
-export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-offscreen}
+export QT_QPA_PLATFORM=offscreen
 
 RUNNER="$PYTHON -m pytest -n auto --dist worksteal"
 
